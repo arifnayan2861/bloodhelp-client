378a558e370e8038136f37faf18ec2eb8a1f7a3c
arifnayan2861 <am.nayan2861@gmail.com>
2024-05-31 20:47:13 +0600

    routes & context added

---
+++ src/context/AuthProvider.jsx
@@ -0,0 +1,93 @@
+import { createContext, useEffect, useState } from "react";
+import {
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import PropTypes from "prop-types";
+
+import auth from "../../firebase.config";
+import axios from "axios";
+
+export const AuthContext = createContext(null);
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const googleProvider = new GoogleAuthProvider();
+
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const loginUser = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const logoutUser = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+
+  const loginWithGoogle = () => {
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      const userEmail = currentUser?.email || user?.email;
+      const loggedUser = { email: userEmail };
+      setUser(currentUser);
+      setLoading(false);
+      if (currentUser) {
+        axios
+          .post(`${import.meta.env.VITE_BACKEND_URL}/jwt`, loggedUser, {
+            withCredentials: true,
+          })
+          .then((res) => {
+            setUser(currentUser);
+            setLoading(false);
+          })
+          .catch((error) => console.log(error));
+      } else {
+        axios
+          .post(`${import.meta.env.VITE_BACKEND_URL}/logout`, loggedUser, {
+            withCredentials: true,
+          })
+          .then((res) => {
+            setUser(null);
+            setLoading(false);
+          })
+          .catch((error) => console.log(error));
+      }
+    });
+
+    return () => {
+      unSubscribe();
+    };
+  }, []);
+  const authInfo = {
+    user,
+    setUser,
+    loading,
+    createUser,
+    loginUser,
+    logoutUser,
+    loginWithGoogle,
+  };
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+AuthProvider.propTypes = {
+  children: PropTypes.node,
+};
+
+export default AuthProvider;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import "./index.css";
+import AuthProvider from "./context/AuthProvider.jsx";
+import routes from "./routes/Routes.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <AuthProvider>
+      <RouterProvider router={routes} />
+      <Toaster />
+    </AuthProvider>
+  </React.StrictMode>
+);
