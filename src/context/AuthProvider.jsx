d34f3f475e6c38caace6d60ddc611cd0fefc874e
arifnayan2861 <am.nayan2861@gmail.com>
2024-06-02 20:36:13 +0600

    login page added

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -11,13 +11,14 @@ import {
 import PropTypes from "prop-types";
 
 import auth from "../../firebase.config";
-import axios from "axios";
+import useAxiosPublic from "../hooks/useAxiosPublic";
 
 export const AuthContext = createContext(null);
 
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
+  const axiosPublic = useAxiosPublic();
 
   const googleProvider = new GoogleAuthProvider();
 
@@ -47,39 +48,62 @@ const AuthProvider = ({ children }) => {
     });
   };
 
+  // useEffect(() => {
+  //   const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+  //     const userEmail = currentUser?.email || user?.email;
+  //     const loggedUser = { email: userEmail };
+  //     setUser(currentUser);
+  //     setLoading(false);
+  //     if (currentUser) {
+  //       axios
+  //         .post(`${import.meta.env.VITE_BACKEND_URL}/jwt`, loggedUser, {
+  //           withCredentials: true,
+  //         })
+  //         .then((res) => {
+  //           setUser(currentUser);
+  //           setLoading(false);
+  //         })
+  //         .catch((error) => console.log(error));
+  //     } else {
+  //       axios
+  //         .post(`${import.meta.env.VITE_BACKEND_URL}/logout`, loggedUser, {
+  //           withCredentials: true,
+  //         })
+  //         .then((res) => {
+  //           setUser(null);
+  //           setLoading(false);
+  //         })
+  //         .catch((error) => console.log(error));
+  //     }
+  //   });
+
+  //   return () => {
+  //     unSubscribe();
+  //   };
+  // }, []);
+
   useEffect(() => {
-    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
-      const userEmail = currentUser?.email || user?.email;
-      const loggedUser = { email: userEmail };
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
-      setLoading(false);
       if (currentUser) {
-        axios
-          .post(`${import.meta.env.VITE_BACKEND_URL}/jwt`, loggedUser, {
-            withCredentials: true,
-          })
-          .then((res) => {
-            setUser(currentUser);
+        const userInfo = { email: currentUser.email };
+        axiosPublic.post("/jwt", userInfo).then((res) => {
+          if (res.data.token) {
+            localStorage.setItem("access-token", res.data.token);
             setLoading(false);
-          })
-          .catch((error) => console.log(error));
+          }
+        });
       } else {
-        axios
-          .post(`${import.meta.env.VITE_BACKEND_URL}/logout`, loggedUser, {
-            withCredentials: true,
-          })
-          .then((res) => {
-            setUser(null);
-            setLoading(false);
-          })
-          .catch((error) => console.log(error));
+        // TODO: remove token (if token stored in the client side: Local storage, caching, in memory)
+        localStorage.removeItem("access-token");
+        setLoading(false);
       }
     });
-
     return () => {
-      unSubscribe();
+      return unsubscribe();
     };
-  }, []);
+  }, [axiosPublic]);
+
   const authInfo = {
     user,
     setUser,
